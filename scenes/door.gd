d46e1a5b3b80b2675b3e70e17d9109fc38b2f3d3
Keneth <166479255+KenyIzConfused@users.noreply.github.com>
2026-03-11 21:39:19 +0800

    door pt 2

---
+++ scenes/door.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		get_node("AnimationPlayer").play("door_open")
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		get_node("AnimationPlayer").play_backwards("door_open")
--- scenes/proto_map.gd
+++ scenes/proto_map.gd
@@ -16,16 +16,3 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		get_tree().change_scene_to_file("res://scenes/proto_map.tscn")
-
-
-func _on_house_demo_039_body_entered(body: Node3D) -> void:
-	print("body detected 1")
-	if body.name == "Player":
-		animation_player.play("door_open")
-	
-
-
-func _on_house_demo_039_body_exited(body: Node3D) -> void:
-	print("body detected 2")
-	if body.name == "Player":
-		animation_player.play("RESET")
