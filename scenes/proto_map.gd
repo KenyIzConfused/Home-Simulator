125e1e2fdfd3888e9daf998c3da77b3b5f6f1c54
Keneth <166479255+KenyIzConfused@users.noreply.github.com>
2026-03-11 20:56:52 +0800

    Door Update

--- scenes/proto_map.gd
+++ scenes/proto_map.gd
@@ -1,5 +1,6 @@
 extends Node3D
 @onready var player: CharacterBody3D = $Player
+@onready var animation_player: AnimationPlayer = $"whatever/2/House_Demo_039/AnimationPlayer"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +16,16 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		get_tree().change_scene_to_file("res://scenes/proto_map.tscn")
+
+
+func _on_house_demo_039_body_entered(body: Node3D) -> void:
+	print("body detected 1")
+	if body.name == "Player":
+		animation_player.play("door_open")
+	
+
+
+func _on_house_demo_039_body_exited(body: Node3D) -> void:
+	print("body detected 2")
+	if body.name == "Player":
+		animation_player.play("RESET")
